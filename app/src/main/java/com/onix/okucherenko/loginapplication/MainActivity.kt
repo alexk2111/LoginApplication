aabad1b38f5079a0157b79077507b2878af5e59d
Aleksandr Kucherenko <alexk2111@ukr.net>
2022-05-27 00:00:20 +0300

    Initial commit

---
+++ app/src/main/java/com/onix/okucherenko/loginapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.onix.okucherenko.loginapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

571548da567b8567fff1a51c97c236450dea1176
Aleksandr Kucherenko <alexk2111@ukr.net>
2022-06-25 09:13:55 +0300

    Finish commit

--- app/src/main/java/com/onix/internship/okucherenko/di/ViewModelModule.kt
+++ app/src/main/java/com/onix/internship/okucherenko/di/ViewModelModule.kt
@@ -2,10 +2,12 @@ package com.onix.internship.okucherenko.di
 
 import com.onix.internship.okucherenko.ui.main.MainViewModel
 import com.onix.internship.okucherenko.ui.splash.SplashViewModel
+import com.onix.internship.okucherenko.ui.wifimanager.ManagerViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    viewModel { ManagerViewModel() }
     viewModel { SplashViewModel() }
     viewModel { MainViewModel() }
 
---
+++ app/src/main/java/com/onix/internship/okucherenko/domain/entity/WiFiDot.kt
@@ -0,0 +1,3 @@
+package com.onix.internship.okucherenko.domain.entity
+
+data class WiFiDot(val name: String, val level: Int)
